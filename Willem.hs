d7e8f4db07028983545fa3b7f5a8ec88afd294bc
Ignat Insarov <kindaro@gmail.com>
2019-09-04 14:13:45 +0300

    Add the version of Willem.

---
+++ Willem.hs
@@ -0,0 +1,18 @@
+module Willem where
+
+import Data.List (sort)
+
+paths1 :: Ord a => (a, a) -> (a, a) -> [(a, a)] -> [[(a, a)]]
+paths1 (a, f) (b, c) _ | c >= f = [[]]
+paths1 _ _ [] = []
+paths1 r s@(b, c) ((d, e):xs) | d > c = []
+                              | d <= b || e <= c = paths1 r s xs
+paths1 r s (x:xs) = map (x:) (paths1 r x xs) ++ paths1 r s xs
+
+paths0 :: Ord a => (a, a) -> [(a, a)] -> [[(a, a)]]
+paths0 (a, _) ((b, _):_) | b > a = []
+paths0 r@(a, _) ((_, c):xs) | c <= a = paths0 r xs
+paths0 r (x:xs) = map (x:) (paths1 r x xs) ++ paths0 r xs
+
+paths :: Ord a => (a, a) -> [(a, a)] -> [[(a, a)]]
+paths = (. sort) . paths0
